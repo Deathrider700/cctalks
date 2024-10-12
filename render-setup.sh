2b29bff1d2d2bb4d86725712db08d27d5fc4b664
Deathrider700 <131989987+Deathrider700@users.noreply.github.com>
2024-10-12 07:45:35 +0530

    Create render-setup.sh

---
+++ render-setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Install Google Chrome
+curl -O https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+apt-get install -y ./google-chrome-stable_current_amd64.deb
+
+# Install ChromeDriver
+CHROME_DRIVER_VERSION=`curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE`
+wget -N http://chromedriver.storage.googleapis.com/$CHROME_DRIVER_VERSION/chromedriver_linux64.zip
+unzip chromedriver_linux64.zip
+chmod +x chromedriver
+mv -f chromedriver /usr/local/bin/chromedriver
+
+# Ensure dependencies are installed
+pip install -r requirements.txt
